c0adf2419bd3d3a80ee12c637cfa054d1b1facb2
Jaye-python <85282656+Jaye-python@users.noreply.github.com>
2022-01-07 13:06:36 +0100

    Update views.py

--- views.py
+++ views.py
@@ -7,6 +7,7 @@ from .serializers import GitHubUsersSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.views import APIView
 
 # Create your views here.
